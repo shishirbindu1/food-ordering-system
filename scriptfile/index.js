8119fdb4c9810d1c8818bcc2ab16c5ef502561c7
bindushishir1 <bindushishir004@gmail.com>
2026-06-26 14:45:36 +0545

    feat:functionality of cart and grand total added

--- scriptfile/index.js
+++ scriptfile/index.js
@@ -1,11 +1,13 @@
 import menuItems from "./menuData.js";
 const tableContainer = document.querySelector(".totalTable");
-const wrapMenu = document.querySelector('.wrapMenu')
+const wrapMenu = document.querySelector(".wrapMenu");
 let backButton = document.querySelector(".backButton");
 let selectedTable = document.querySelector(".selectedTable");
 let menu = document.querySelector(".menu");
-let table;
+const cartItems = document.querySelector(".cartItems");
+const totalPrice = document.querySelector(".totalPrice");
 let tableNo = null;
+let cart = [];
 
 for (let i = 1; i <= 10; i++) {
   const tableDiv = document.createElement("div");
@@ -27,13 +29,13 @@ menuItems.forEach((item) => {
     <div class="mt-3 flex justify-between items-center">
         <span class="font-bold">Rs. ${item.price}</span>
 
-        <button class="px-3 py-1 bg-green-500 text-white rounded">
+        <button type= "text" class="addToCart px-3 py-1 bg-green-500 text-white rounded" data-item-Id="${item.id}">
             Add
         </button>
     </div>
 `;
 
-wrapMenu.appendChild(card);
+  wrapMenu.appendChild(card);
 });
 
 tableContainer.addEventListener("click", (e) => {
@@ -42,7 +44,6 @@ tableContainer.addEventListener("click", (e) => {
   if (!e.target.classList.contains("table")) return;
 
   let table = e.target.closest(".table");
-  console.log(table);
   activeTable(table);
 
   tableNo = table.dataset.table;
@@ -64,4 +65,44 @@ function activeTable(tableDiv) {
   });
 
   tableDiv.classList.add("bg-green-500");
+}
+
+//add to cart
+const addToCart = document.querySelectorAll(".addToCart");
+
+addToCart.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const id = Number(e.target.dataset.itemId);
+    let selectedItem = null;  
+    menuItems.forEach((item) => {
+     
+      if (item.id === id) {
+        selectedItem = item;
+      }
+      
+    });
+    if (selectedItem) {
+      cart.push(selectedItem);
+      renderCart();
+    }
+  });
+});
+
+function renderCart() {
+  cartItems.innerHTML = "";
+  let total = 0;
+  cart.forEach((items) => {
+
+    const row = document.createElement("tr");
+    row.innerHTML = `
+    <td>${items.name}</td>
+    <td>1</td>
+    <td>${items.price}</td>
+    <td>${items.price}</td>
+    `;
+
+    cartItems.appendChild(row);
+    total += items.price 
+    totalPrice.innerText = total
+  });
 }
